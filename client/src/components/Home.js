776f4a6023645e4b274da9e64d0fcdb90c68c7b1
blankmaru <coopskywavy@gmail.com>
2020-10-11 13:42:46 +0300

    Starting fixing bugs....

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,25 @@
 import React from 'react'
-import { Header } from 'semantic-ui-react'
+import { Header, Button } from 'semantic-ui-react'
 
-const Home = () => (
-  <div style={{marginTop: '2rem'}}>
-    <Header size='medium'>Home Page</Header>
-  </div>
-)
+import axios from 'axios'
+
+const Home = () => {
+  const getData = (e) => {
+    e.preventDefault();
+
+    axios.get("/api/users").then(res => {
+      console.log(res.data);
+    });
+  };
+
+  return(
+    <div style={{marginTop: '2rem'}}>
+      <Header size='medium'>Home Page</Header>
+      <Button onClick={getData}>
+        GET
+      </Button>
+    </div>
+  )
+} 
 
 export default Home
